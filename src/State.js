556acab27cd6beb98d59fa4e8caae96cef75fdff
Yurematias <yurematias@ufrn.edu.br>
2020-03-21 14:17:23 -0300

    estilos atualizados

--- src/State.js
+++ src/State.js
@@ -20,41 +20,45 @@ class State {
             this.color = '#770000';
             this.fillColor = "#770000";
         } else if (this.confirmedCases > 15000) {
-            this.radius = 140000;
+            this.radius = 250000;
             this.color = '#880000';
             this.fillColor = "#880000";
         } else if (this.confirmedCases > 5000) {
-            this.radius = 120000;
+            this.radius = 200000;
             this.color = '#990000';
             this.fillColor = "#990000";
         } else if (this.confirmedCases > 2000) {
-            this.radius = 100000;
+            this.radius = 170000;
             this.color = '#990000';
             this.fillColor = "#990000";
         } else if (this.confirmedCases > 1000) {
-            this.radius = 90000;
+            this.radius = 150000;
             this.color = '#AA0000';
             this.fillColor = "#AA0000";
         } else if (this.confirmedCases > 500) {
-            this.radius = 85000;
+            this.radius = 140000;
             this.color = '#BB0000';
             this.fillColor = "#BB0000";
         } else if (this.confirmedCases > 200) {
-            this.radius = 80000;
+            this.radius = 120000;
             this.color = '#CC0000';
             this.fillColor = "#CC0000";
-        } else if (this.confirmedCases > 50) {
-            this.radius = 65000;
+        } else if (this.confirmedCases > 100) {
+            this.radius = 100000;
             this.color = '#DD0000';
             this.fillColor = "#DD0000";
-        } else if (this.confirmedCases > 25) {
-            this.radius = 55000;
+        } else if (this.confirmedCases > 40) {
+            this.radius = 70000;
             this.color = '#EE0000';
             this.fillColor = "#EE0000";
-        } else {
-            this.radius = 40000;
+        } else if (this.confirmedCases > 20) {
+            this.radius = 50000;
             this.color = '#FF0000';
             this.fillColor = "#FF0000";
+        } else {
+            this.radius = 20000;
+            this.color = '#FF3333';
+            this.fillColor = "#FF3333";
         }
     }
 }
