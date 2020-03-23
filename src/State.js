709ba87525416dc6d4aaf9a6ac8d4a1c0364cf7f
Yurematias <yurematias@ufrn.edu.br>
2020-03-22 21:19:43 -0300

    sizes of circles updated

--- src/State.js
+++ src/State.js
@@ -17,35 +17,35 @@ class State {
 
     setStyle() {
         if (this.confirmedCases > 30000) {
-            this.radius = 300000;
+            this.radius = 350000;
             this.color = '#770000';
             this.fillColor = "#770000";
         } else if (this.confirmedCases > 15000) {
-            this.radius = 250000;
+            this.radius = 300000;
             this.color = '#880000';
             this.fillColor = "#880000";
         } else if (this.confirmedCases > 5000) {
-            this.radius = 200000;
+            this.radius = 250000;
             this.color = '#990000';
             this.fillColor = "#990000";
         } else if (this.confirmedCases > 2000) {
-            this.radius = 170000;
+            this.radius = 210000;
             this.color = '#990000';
             this.fillColor = "#990000";
         } else if (this.confirmedCases > 1000) {
-            this.radius = 150000;
+            this.radius = 190000;
             this.color = '#AA0000';
             this.fillColor = "#AA0000";
         } else if (this.confirmedCases > 500) {
-            this.radius = 140000;
+            this.radius = 160000;
             this.color = '#BB0000';
             this.fillColor = "#BB0000";
         } else if (this.confirmedCases > 200) {
-            this.radius = 120000;
+            this.radius = 130000;
             this.color = '#CC0000';
             this.fillColor = "#CC0000";
         } else if (this.confirmedCases > 100) {
-            this.radius = 100000;
+            this.radius = 110000;
             this.color = '#DD0000';
             this.fillColor = "#DD0000";
         } else if (this.confirmedCases > 40) {
