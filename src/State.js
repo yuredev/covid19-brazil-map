e6de1032a514e8233c745054a96ff1cc5e943b93
yuredev <yurematias@ufrn.edu.br>
2020-12-03 17:52:23 -0300

    update circule of infection size

--- src/State.js
+++ src/State.js
@@ -4,6 +4,8 @@ class State {
   constructor(posX, posY, confirmedCases, suspiciousCases, deaths) {
     this["lat-lng"] = latLng(posY, posX);
     this.fillOpacity = 0.5;
+    this.color = 'rgb(90, 0, 40)';
+    this.fillColor = "rgb(100, 0, 30)";
     this.confirmedCases = confirmedCases;
     this.suspiciousCases = suspiciousCases;
     this.deaths = deaths;
@@ -14,62 +16,12 @@ class State {
     this.setStyle();
   }
   setStyle() {
-    if (this.confirmedCases > 100000) {
-      this.radius = 400000;
-      this.color = 'rgb(90, 0, 40)';
-      this.fillColor = "rgb(100, 0, 30)";
-    } else if (this.confirmedCases > 80000) {
-      this.radius = 360000;
-      this.color = 'rgb(100, 0, 30)';
-      this.fillColor = "rgb(100, 0, 30)";
-    } else if (this.confirmedCases > 60000) {
-      this.radius = 310000;
-      this.color = 'rgb(120, 0, 20)';
-      this.fillColor = "rgb(120, 0, 20)";
-    } else if (this.confirmedCases > 50000) {
-      this.radius = 290000;
-      this.color = 'rgb(150, 0, 10)';
-      this.fillColor = "rgb(150, 0, 10)";
-    } else if (this.confirmedCases > 40000) {
-      this.radius = 250000;
-      this.color = 'rgb(150, 0, 10)';
-      this.fillColor = "rgb(150, 0, 10)";
-    } else if (this.confirmedCases > 35000) {
-      this.radius = 200000;
-      this.color = 'rgb(170, 0, 0)';
-      this.fillColor = "rgb(170, 0, 0)";
-    } else if (this.confirmedCases > 25000) {
-      this.radius = 150000;
-      this.color = 'rgb(180, 0, 0)';
-      this.fillColor = "rgb(180, 0, 0)";
-    } else if (this.confirmedCases > 15000) {
-      this.radius = 100000;
-      this.color = 'rgb(195, 0, 0)';
-      this.fillColor = "rgb(195, 0, 0)";
-    } else if (this.confirmedCases > 10000) {
-      this.radius = 80000;
-      this.color = 'rgb(205, 10, 0)';
-      this.fillColor = "rgb(205, 10, 0)";
-    } else if (this.confirmedCases > 5000) {
-      this.radius = 50000;
-      this.color = 'rgb(215, 20, 0)';
-      this.fillColor = "rgb(215, 20, 0)";
-    } else if (this.confirmedCases > 2000) {
-      this.radius = 25000;
-      this.color = 'rgb(225, 30, 0)';
-      this.fillColor = "rgb(225, 30, 0)";
-    } else if (this.confirmedCases > 1000) {
-      this.radius = 10000;
-      this.color = 'rgb(235, 40, 0)';
-      this.fillColor = "rgb(235, 40, 0)";
-    } else if (this.confirmedCases > 500) {
-      this.radius = 7500;
-      this.color = 'rgb(245, 50, 0)';
-      this.fillColor = "rgb(245, 50, 0)";
+    if (this.confirmedCases < 40000) {
+      this.radius = 40000;
+    } else if (this.confirmedCases > 550000) {
+      this.radius = 550000;
     } else {
-      this.radius = 5000;
-      this.color = 'rgb(255, 60, 0)';
-      this.fillColor = "rgb(255, 60, 0)";
+      this.radius = this.confirmedCases ? this.confirmedCases : 0;  
     }
   }
 }
